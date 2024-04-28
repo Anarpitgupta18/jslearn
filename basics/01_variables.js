204ce0997eef5b392e1d89a0953e707138960742
Anarpitgupta18 <139436232+Anarpitgupta18@users.noreply.github.com>
2024-04-28 06:14:38 +0000

    datatypes

---
+++ basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=1233
+let accountEmail="arpit123@gmail.com"
+var accountPassword="1111"
+accountCity ="Indore"
+
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+
+*/
+console.table([accountEmail,accountId,accountPassword,accountCity]);
---
+++ basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"set alert"
+//treat all JS cide as newer version
+// alert(3+3) //we are using nodejs not browser
+console.log(3 + 3)
+console.log("Arpit")
+
+
+
+
+
+
+let name="Arpit" 
+let age =18
+let isLoggedIn=false
+
+//number --2 to power 53
+//bigint
+//string =""
+// Boolean ==> true/false
+// null => standalone value
+// undefined =abhi value assign nhii ki gyii hai 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
